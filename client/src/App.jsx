d615ff8cf7bb8d9323bda26bcb3b5c69c9b1b84f
Zhiqiao Gong <zqgong86@gmail.com>
2026-05-26 23:18:44 -0700

    Adjust desktop and mobile navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,9 @@ import MessageSection from './components/MessageSection'
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-white">
+    <div className="min-h-screen bg-gray-900 text-white md:flex md:flex-row">
       <Sidebar />
-
-      {/* Mobile Header */}
-      <main className="pt-28 md:pt-0 md:ml-64 flex-1 p-8 max-w-5xl">
+      <main className="pt-28 md:pt-0 flex-1 p-8 max-w-5xl">
         <ExperienceSection />
         <ProjectSection />
         <MessageSection />
