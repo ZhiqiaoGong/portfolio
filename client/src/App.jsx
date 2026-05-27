2b12d2ad5184f9ac20bb2cac0fc4e64a2fd708d4
Zhiqiao Gong <zqgong86@gmail.com>
2026-05-26 23:37:47 -0700

    Adjust padding in main section of App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,12 +7,10 @@ export default function App() {
   return (
     <div className="min-h-screen bg-gray-900 text-white md:flex">
       <Sidebar />
-      <main className="pt-36 md:pt-0 flex-1 min-w-0 px-12 py-8">
-        <div className="max-w-3xl ml-12">
-          <ExperienceSection />
-          <ProjectSection />
-          <MessageSection />
-        </div>
+      <main className="pt-36 md:pt-0 flex-1 min-w-0 px-16 py-8">
+        <ExperienceSection />
+        <ProjectSection />
+        <MessageSection />
       </main>
     </div>
   )
